76256c494ff5b17787543eb469d567315c562b70
shubbhu <shubbhumourya@gmail.com>
2021-11-24 20:31:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
+  const [markDown, setMarkDown] = useState(`
+
+  this is a peragraph
+
+  **this is bolded text**
+
+  > Block Quotes
+  
+  # Heading
+
+  ## Heading 2
+  
+  1. list item 1
+  2. list item 2 
+  3. list item 3
+  
+  [visit my website](https://shubbhu.online)
+
+  this is inline \`<div>Inline code</div>\`
+
+  this is a block of code
+
+  \`\`\`
+    let x = 1;
+    let y = 2;
+    let z = x + y;
+  \`\`\`
+
+  ![nature](https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg)
+  
+  `);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <textarea
+          id="editor"
+          value={markDown}
+          onChange={(e) => {
+            setMarkDown(e.target.value);
+          }}
+        ></textarea>
+        <div id="preview">
+          <ReactMarkdown>{markDown}</ReactMarkdown>
+        </div>
       </header>
     </div>
   );
